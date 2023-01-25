2ad666c1a4823d0a6536d0f4ef4d558d70683341
annendinguri <annewaithira098@gmail.com>
2023-01-25 13:09:16 +0300

    create react application to demonstrate routing

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About =()=> {
+    return(
+        <div className ="ui raised very padded text containr segment"
+        style={{marginTop: '80px'}}>
+            <h3 className="ui header" >About</h3>
+            <p> LOREM IPSUM
+
+            </p>
+
+        </div>
+    )
+}
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CONTACT =()=> {
+    return(
+        <div className ="ui raised very padded text containr segment"
+        style={{marginTop: '80px'}}>
+            <h3 className="ui header" >Home</h3>
+            <p> LOREM IPSUM
+
+            </p>
+
+        </div>
+    )
+}
+export default CONTACT;
