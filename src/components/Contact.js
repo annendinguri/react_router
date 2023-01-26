6db78b338f2561d58c54c02cc0ee62e48ecc5299
annendinguri <annewaithira098@gmail.com>
2023-01-26 12:45:38 +0300

    update react router app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const CONTACT =()=> {
+const Contact =()=> {
     return(
-        <div className ="ui raised very padded text containr segment"
+        <div className ="ui raised very padded text container segment"
         style={{marginTop: '80px'}}>
-            <h3 className="ui header" >Home</h3>
+            <h3 className="ui header" >Contact</h3>
             <p> LOREM IPSUM
 
             </p>
@@ -12,4 +12,4 @@ const CONTACT =()=> {
         </div>
     )
 }
-export default CONTACT;
+export default Contact;
